67b36a0b45a903a8e48191e7809e0dc8020a1964
breonw <breon_williams@unc.edu>
2016-10-08 18:18:17 -0400

    add courses

--- archive-courses.php
+++ archive-courses.php
@@ -11,7 +11,7 @@ get_header();
  * determine main column size from actived sidebar
  */
 $main_column_size = bootstrapBasicGetMainColumnSize();
-?> 
+?>
 <?php get_sidebar('left'); ?>
 				<div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
 					<main id="main" class="site-main" role="main">
