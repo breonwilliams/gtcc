eeb282350d433e00a340246858c4e473e53806c2
breonw <breon_williams@unc.edu>
2016-11-19 08:28:26 -0500

    modify shortcode

--- content-courses.php
+++ content-courses.php
@@ -3,13 +3,6 @@
 		<strong><a href="<?php the_permalink(); ?>">
 			<?php the_title(); ?>
 		</a></strong>
-		<?php
-		$gtcc_register = get_theme_mod( 'registration_textbox', '' );
-		if($gtcc_register) { ?>
-			<p><a href="<?php echo $gtcc_register; ?>" class="btn btn-primary btn-sm btn-bordered margintop-15" target="_blank">Register <i class="fa fa-chevron-right"></i></a></p>
-		<?php } else { ?>
-
-		<?php } ?>
 	</td>
 	<td>
 		<?php list_hierarchical_terms(); ?>
@@ -19,5 +12,12 @@
 	</td>
 	<td>
 		<?php the_field( 'course_number' ); ?>
+		<?php
+		$gtcc_register = get_theme_mod( 'registration_textbox', '' );
+		if($gtcc_register) { ?>
+			<p><a href="<?php echo $gtcc_register; ?>" class="btn btn-primary btn-sm btn-bordered margintop-15" target="_blank">Register <i class="fa fa-chevron-right"></i></a></p>
+		<?php } else { ?>
+
+		<?php } ?>
 	</td>
 </tr>
